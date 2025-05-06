0cd2f433d41bc941582e853c40f783c490789801
Agenor <agenoralencaar@gmail.com>
2025-05-05 23:59:32 -0300

    base do front com angular e + Tailwind

---
+++ win-frontend/src/app/components/categorias/categorias.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriasComponent } from './categorias.component';
+
+@NgModule({
+  declarations: [CategoriasComponent],
+  imports: [CommonModule],
+  exports: [CategoriasComponent]
+})
+export class CategoriasModule {}
