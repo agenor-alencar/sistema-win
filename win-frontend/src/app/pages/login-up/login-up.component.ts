2ba62f0c2fc8994601149997b5e12b38ed19b977
tchezery <tchezeryribeiro@gmail.com>
2025-05-07 00:05:00 -0300

    add/global-styles

---
+++ win-frontend/src/app/pages/login-up/login-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-up',
+  imports: [],
+  templateUrl: './login-up.component.html',
+  styleUrl: './login-up.component.css'
+})
+export class LoginUpComponent {
+
+}
